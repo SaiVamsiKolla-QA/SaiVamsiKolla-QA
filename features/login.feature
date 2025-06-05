e422a2abe099006834ab438df14f0a7c07ae2f1a
Sai Vamsi Kolla <saivamsikolla@gmail.com>
2025-06-05 09:42:25 -0600

    Delete features directory

--- features/login.feature
+++
@@ -1,4 +0,0 @@
-Feature: Login
-  Scenario: Dummy login
-    Given I open a blank page
-    Then the title should contain ""
--- features/steps/login_steps.py
+++
@@ -1,9 +0,0 @@
-from behave import given, then
-
-@given('I open a blank page')
-def step_open_blank(context):
-    context.browser.get('about:blank')
-
-@then('the title should contain ""')
-def step_title(context):
-    assert context.browser.title == ''
